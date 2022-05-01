c5a582f5458e2b697fcb5e3c0a5d44436c65e9f0
Mariano Gamba <mariano.gamba77@gmail.com>
2022-05-01 19:02:39 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+const async = require('hbs/lib/async');
+var router = express.Router();
+var nodemailer = require('nodemailer')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+varnodemailer = require("nodemailer");
+
+router.post('/', async(req, res, next) => {
+
+    var nombre = req.body.nombre;
+    var apellido = req.body.apellido;
+    var mail = req.body.mail;
+    var escribinos = req.body.escribinos;
+    var telefono = req.body.telefono;
+
+    console.log(req. body);
+
+    var obj = {
+      to: 'mariano.gamba77@gmail.com',
+      subject: 'CONTACTO WEB',
+      html: nombre + "se contacto a través de la web y quiere más información a este correo : " + mail + ".<br> Además, hizo este comentario : " + mensaje + ".<br> Su tel es: " + telefono
+    }
+
+    var transport = nodemailer.createTransport({
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+      }
+    });
+
+    var info = await transport.sendMail(obj);
+
+    res.render('contacto', {
+      message: 'Mensaje enviado correctamente'
+    });
+});
+
+
+module.exports = router;
