97287d25ac4c9b75ca2bf10f09ca1c25ad9dc589
Mariano Gamba <mariano.gamba77@gmail.com>
2022-06-05 18:52:55 -0300

    final trabajo unidad 4

--- routes/admin/login.js
+++ routes/admin/login.js
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next) {
    });
 });
 
+router.get('/logout', function (req, res, next){
+      req.session.destroy();
+      res.render('admin/login', {
+         layout: 'admin/layout'
+      });
+});
 
 
 router.post('/', async (req, res, next) =>{
@@ -21,6 +27,9 @@ router.post('/', async (req, res, next) =>{
          var data = await usuariosModel.getUserAndPassword(usuario, password);
 
          if (data != undefined) {
+            req.session.id_usuario = data.id;
+            req.session.nombre =data.usuario;
+
             res.redirect('/admin/novedades');
          } else{
             res.render('admin/login', {
