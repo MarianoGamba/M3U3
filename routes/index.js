3e4155ca0eb760b44662763911ae42982897dcbd
nicog28 <nicolas_gamba@hotmail.com>
2022-06-12 19:04:14 -0300

    cambios grid parte 1

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var nodemailer = require('nodemailer')
 var novedadesModel = require('../models/novedadesModel');
 /* GET home page. */
 router.get('/', async function(req, res, next){
-  novedades = await novedadesModel.getNovedades();
+   var novedades = await novedadesModel.getNovedades();
   novedades = novedades.splice(0,5);
   res.render('index',{
     novedades
